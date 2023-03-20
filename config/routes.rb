fb011985a0bcaa8a4375c0a2aefe4ebbeda60ef0
reaganongaya <reagan.ongaya@student.moringaschool.com>
2023-03-20 21:51:50 +0300

    Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,5 @@ Rails.application.routes.draw do
   resources :movies, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+get '/movies/:id/summary', to: 'movies#summary'
